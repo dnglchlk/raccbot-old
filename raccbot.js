60106769a22046635f85c8c4033b8435b00c784e
dnglchlk <dnglchlk@helios>
2020-10-30 02:17:56 -0700

    raccbot now has reactions to msgs, join/leave/ban+

--- raccbot.js
+++ raccbot.js
@@ -7,9 +7,8 @@ const mem = osu.mem;
 const netstat = osu.netstat;
 const os = require('os');
 const Discord = require('discord.js');
-const { cpuUsage } = require('process');
-const { info } = require('console');
 const client = new Discord.Client();
+const raccArr = ['raccoon', 'racc', 'raccbot', 'trash', 'trash panda', 'egg', 'eggs', 'ĕğğ', 'dnglchlk', 'erick', 'Erick', 'ĔĞĞ', 'procyon', 'procyon lotor', 'helios'];
 
 client.on('ready', () => {
     client.user.setStatus('available')
@@ -24,15 +23,56 @@ client.on('ready', () => {
     function aliveTimer() { console.log("raccbot is still alive, carry on."); }
 });
 
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'non-amet');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+});
+
+client.on('guildMemberRemove', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'non-amet');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`${member} has left Chalkland`);
+});
+
+client.on("guildBanAdd", function (guild, user) {
+    if (guild.id === '696540783385247824') {
+        const channel = guild.channels.cache.find(ch => ch.name === 'non-amet');
+        channel.startTyping();
+        channel.send(`${user} was just **banned** from ${guild} :eyes:`);
+        channel.stopTyping();
+    }
+});
+
 client.once('ready', () => {
     console.log('raccbot is ready to steal your trash now 🦝');
 });
 
 client.on('message', async message => {
+    const raccEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'raccmask');
+    async function raccMessage() {
+        for (let i = 0; i < raccArr.length; i++) {
+            if (message.content.includes(raccArr[i]) === true) {
+                try {
+                    await message.react(raccEmoji);
+                    console.log(`did somebody say ${raccArr[i]}?`);
+                } catch (error) {
+                    console.error(`raccEmoji not in ${guild}`)
+                }
+            }
+        }
+    }
     if (message.author.bot) return;
     let { guild, channel } = message;
     let msgavatarurl = message.member.user.avatarURL;
     const usermsg = message.content.split(' ');
+    raccMessage();
 
     switch (usermsg[0]) {
         case 'racc.ping':
@@ -48,11 +88,10 @@ client.on('message', async message => {
             message.channel.stopTyping();
             break;
         case 'racc.repeat':
-            // Rejoins every slice at spaces after the first slice
+        case 'racc.repeat.twice':
             let str = usermsg.slice(1).join(' ');
             message.channel.startTyping();
             var msgSelect = Math.round(Math.random() * (3 - 0) + 1);
-            console.log(`Repeat of ${message.member.user.tag} message ${usermsg.slice(1).join(' ')} from ${channel}, ${guild}`);
             if ((message.member.user.id).toString() != "575763130743914496") {
                 str = str.replace(/erick is/i, `${message.member.user.tag} is`);
             }
@@ -69,6 +108,11 @@ client.on('message', async message => {
                 default:
                     break;
             }
+            if (usermsg[0] === "racc.repeat.twice") {
+                str = `${str}${str}`;
+                console.log(`Double repeat of ${message.member.user.tag} message ${usermsg.slice(1).join(' ')} from ${channel}, ${guild}`);
+            }
+            else { console.log(`Repeat of ${message.member.user.tag} message ${usermsg.slice(1).join(' ')} from ${channel}, ${guild}`); }
             message.channel.send(str);
             message.channel.stopTyping();
             break;
@@ -99,11 +143,15 @@ client.on('message', async message => {
             //console.log(processorCount, processorUsage, memoryTotal, memoryFree, networkUsage, systemUptime)
             message.channel.send(heliosinfEmbed);
             message.channel.stopTyping();
-            console.log(`Finished collecting helios system data, sent to ${channel}, ${guild}`)
+            console.log(`Finished collecting helios system data, sent to ${channel}, ${guild}`);
             break;
+        case 'racc.info':
+        case 'racc.commands':
+        case 'racc.cmds':
         case 'racc.help':
             message.channel.startTyping();
             message.channel.send("Find Help Here: https://chalkland.net/raccbot/help.html");
+            console.log(`Help page sent to ${channel}, ${guild}`);
             message.channel.stopTyping();
             break;
         default:
